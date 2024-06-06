a23c645a9afe9629cdcd96824fb1f756c6454701
Anirudh Singh Rajawat <anisingh2708@gmail.com>
2024-06-06 15:11:10 +0530

    added Buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet, Button } from 'react-native';
+import React,{useState} from 'react';
+import { SafeAreaView, View, Text, StyleSheet, Button,TextInput } from 'react-native';
 import { red } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 import Constants from 'expo-constants'
-import GlobalStyles from '@/components/GlobalStyles';
+import GlobalStyles from '@/components/GlobalStyles'
+import exstyles from '../styles';
+
+
 
 
 
 export default function MainScreen() {
+  const[name,setName]=useState("");
 
-  const apple =(val: string)=>{
-    console.warn(val)
-  }
+  
   return(<SafeAreaView style={GlobalStyles.droidSafeArea}>
-    <View style={{flex:1, flexDirection:'column',}}>
-   <View style={{alignSelf:'center'}}>
-    <Text style={{fontSize:35,paddingBottom:10}}>Weather App</Text>
-    <Text style={{marginTop:50,fontSize:25}}>City-Indore</Text>
-    <Text style={{fontSize:25}}>Temperature-40C</Text>
-    </View>
-    <View style={{marginTop:550}}>
-   <Button title='Update' onPress={()=>apple("Anirudh")} color={'steelblue'}/>
-   </View>
+    <View style={{flex:1,flexDirection:'column'}}>
+   <View style={{paddingTop:30,alignItems:'center'}}>
+    <Text style={styles.heading}>Weather App</Text>
+    <Text style={exstyles.text}>City-Indore</Text>
+    <Text style={[exstyles.text]}>Temperature-40C</Text>
+    <Text style={[exstyles.text]}>My name is:{name}</Text>
+    <TextInput
+    style={styles.InputBox}
+    placeholder='Enter Your Name'
+    
+    onChangeText={(text=>setName(text))}
+    value={name}
+    />
+    <Button title='Clear Value' onPress={()=>setName('')}/>
+    
+
     
     </View>
+   
+    
+     
+
+    </View>
+    
 
     
 </SafeAreaView>
@@ -33,4 +48,35 @@ export default function MainScreen() {
   );
 }
 
+
+const styles=StyleSheet.create({
+
+  text:{
+    color:'steelblue',
+    fontSize:25,
+    backgroundColor:'yellow',
+    padding:10,
+    marginBottom:10,
+    borderRadius:10,
+    borderWidth:3,
+    borderColor:'black'
+
+  },
+  heading:{
+      fontSize:50,
+      fontWeight:'bold',
+      
+      
+  },
+  InputBox:{
+    fontSize:18,
+    borderWidth:2,
+    color:"black",
+    borderRadius:10,
+    padding:10,
+    marginBottom:10,
+    
+  }
+
+})
 
---
+++ app/styles.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet} from 'react-native';
+
+
+export default
+ exstyles=StyleSheet.create({
+
+    text:{
+      color:'white',
+      fontSize:25,
+      backgroundColor:'black',
+      padding:10,
+      marginBottom:10,
+      borderRadius:10,
+      borderWidth:3,
+      borderColor:'black'
+  
+    },
+    heading:{
+        fontSize:50,
+        fontWeight:'bold',
+        
+        
+    }
+  
+  })
+  
+  
